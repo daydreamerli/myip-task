1b4d25a890b9ecb403ac3791506c2800e594a016
daydreamerli <frankli84@outlook.com>
2021-11-22 08:54:18 +1300

    clean up Appjs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
+import tw from "twin.macro";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  ${tw`
+    w-full
+    h-full
+    flex
+    flex-col
+  `};
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+    {/*react pages/router sits here */} 
+       
+  </AppContainer>
   );
 }
 
